4603a33582b9556356b7ff899c557116fe96cb95
Abdullah Almamun <165108040+almamun-de@users.noreply.github.com>
2025-02-10 23:30:31 +0100

    Update xor_cryptography.py

--- xor_cryptography.py
+++ xor_cryptography.py
@@ -6,11 +6,3 @@ def xor_encrypt_decrypt(input_string, key):
         xor_byte = chr(ord(input_string[i]) ^ ord(key[i % len(key)]))
         output.append(xor_byte)
     return ''.join(output)
-
-if __name__ == "__main__":
-    plaintext = input("Enter plaintext: ")
-    key = input("Enter key: ")
-    ciphertext = xor_encrypt_decrypt(plaintext, key)
-    print(f"Ciphertext: {ciphertext}")
-    decrypted_text = xor_encrypt_decrypt(ciphertext, key)
-    print(f"Decrypted Text: {decrypted_text}")
